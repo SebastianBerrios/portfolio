308cbc6ef074023c1608e11f64c48b87e61731d6
Sebastian <sebberrios99@gmail.com>
2025-03-17 16:38:45 -0500

    feat: add projects or freelances selector

---
+++ src/components/ProjectSelector.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function ProjectSelector({
+  icon,
+  projectsComponent,
+  freelancesComponent,
+}: any) {
+  const [projects, setProjects] = useState<boolean>(true);
+
+  return (
+    <>
+      <div className="flex items-center mb-6 text-3xl font-semibold gap-x-3 text-black/80 dark:text-white">
+        {icon}
+        <button onClick={() => setProjects(true)}>
+          <h2 className={projects ? "font-bold" : "text-gray-600/80"}>
+            Proyectos
+          </h2>
+        </button>
+        <span>/</span>
+        <button onClick={() => setProjects(false)}>
+          <h2 className={projects ? "text-gray-600/80" : "font-bold"}>
+            Freelances
+          </h2>
+        </button>
+      </div>
+      {projects ? projectsComponent : freelancesComponent}
+    </>
+  );
+}
